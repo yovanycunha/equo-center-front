923b9e3481f2ec4e3e7c99d6fac27d53e0b2e950
Yovany Cunha <yovany33@gmail.com>
2024-05-03 07:30:35 -0300

    Cadastro da equipe - refactor dos campos da equipe basica - sprint7

--- src/app/centro/page.tsx
+++ src/app/centro/page.tsx
@@ -4,8 +4,11 @@ import { useForm } from "react-hook-form";
 import scss from "./page.module.scss";
 import Input from "@/components/Input/Input";
 import { ICentro } from "./types";
+import { useState } from "react";
 
 export default function CadastroEquipe() {
+  const [isBasicTeam, setIsBasicTeam] = useState<Boolean>(true);
+
   const {
     register,
     handleSubmit,
@@ -27,56 +30,6 @@ export default function CadastroEquipe() {
     },
   });
 
-  const professionals = {
-    fisio: {
-      nameRef: register("fisioterapeuta.nome", {
-        required: true,
-        minLength: { value: 3, message: "Nome inválido" },
-      }),
-      certifiedRef: register("fisioterapeuta.certified", {
-        required: true,
-      }),
-      professionalIdRef: register("fisioterapeuta.professionalId", {
-        required: true,
-      }),
-    },
-  };
-
-  const psicologoNameRef = register("psicologo.nome", {
-    required: true,
-    minLength: { value: 3, message: "Nome inválido" },
-  });
-
-  const psicologoCertifiedRef = register("psicologo.certified", {
-    required: true,
-  });
-
-  const psicologoProfessionalIdRef = register("psicologo.professionalId", {
-    required: true,
-  });
-
-  const medicoNameRef = register("medico.nome", {
-    required: true,
-    minLength: { value: 3, message: "Nome inválido" },
-  });
-
-  const medicoCertifiedRef = register("medico.certified", {
-    required: true,
-  });
-
-  const medicoProfessionalIdRef = register("medico.professionalId", {
-    required: true,
-  });
-
-  const equitadorNameRef = register("equitador.nome", {
-    required: true,
-    minLength: { value: 3, message: "Nome inválido" },
-  });
-
-  const equitadorCertifiedRef = register("equitador.certified", {
-    required: true,
-  });
-
   const onFisioCertifiedChange = () => {
     setValue("fisioterapeuta.certified", !watch("fisioterapeuta.certified"));
   };
@@ -93,6 +46,123 @@ export default function CadastroEquipe() {
     setValue("equitador.certified", !watch("equitador.certified"));
   };
 
+  const basicTeam = {
+    fisioterapeuta: {
+      nameRef: register("fisioterapeuta.nome", {
+        required: true,
+        minLength: { value: 3, message: "Nome inválido" },
+      }),
+      professionalIdRef: register("fisioterapeuta.professionalId", {
+        required: true,
+      }),
+      certifiedRef: register("fisioterapeuta.certified", {
+        required: true,
+      }),
+      errors: errors.fisioterapeuta,
+      onCertifiedChange: () => onFisioCertifiedChange(),
+      placeholderName: "Nome do Fisioterapeuta",
+      placeholderProfessionalId: "CREFITO",
+    },
+
+    psicologo: {
+      nameRef: register("psicologo.nome", {
+        required: true,
+        minLength: { value: 3, message: "Nome inválido" },
+      }),
+      professionalIdRef: register("psicologo.professionalId", {
+        required: true,
+      }),
+      certifiedRef: register("psicologo.certified", {
+        required: true,
+      }),
+      errors: errors.psicologo,
+      onCertifiedChange: () => onPsicoCertifiedChange(),
+      placeholderName: "Nome do Psicólogo",
+      placeholderProfessionalId: "CRP",
+    },
+
+    medico: {
+      nameRef: register("medico.nome", {
+        required: true,
+        minLength: { value: 3, message: "Nome inválido" },
+      }),
+      professionalIdRef: register("medico.professionalId", {
+        required: true,
+      }),
+      certifiedRef: register("medico.certified", {
+        required: true,
+      }),
+      errors: errors.medico,
+      onCertifiedChange: () => onMedicoCertifiedChange(),
+      placeholderName: "Nome do Médico",
+      placeholderProfessionalId: "CRM",
+    },
+    equitador: {
+      nameRef: register("equitador.nome", {
+        required: true,
+        minLength: { value: 3, message: "Nome inválido" },
+      }),
+      certifiedRef: register("equitador.certified", {
+        required: true,
+      }),
+      errors: errors.equitador,
+      onCertifiedChange: () => onEquitadorCertifiedChange(),
+      placeholderName: "Nome do Equitador",
+    },
+  };
+
+  const renderInput = (professional: any, type: string) => {
+    return (
+      <>
+        <h3 className={scss.professionalTitle}>{type}</h3>
+        <Input
+          name={professional.nameRef.name}
+          placeholder={professional.placeholderName}
+          inputref={professional.nameRef.ref}
+          value={watch(professional.nameRef.name)}
+          onChange={professional.nameRef.onChange}
+          onBlur={professional.nameRef.onBlur}
+          errors={professional.errors?.nome && true}
+          errorMessage={professional.errors?.nome.message}
+        />
+
+        <div className={scss.inlineGroup}>
+          {type !== "Equitador" && (
+            <Input
+              name={professional.professionalIdRef.name}
+              placeholder={professional.placeholderProfessionalId}
+              inputref={professional.professionalIdRef.ref}
+              value={watch(professional.professionalIdRef.name)}
+              onChange={professional.professionalIdRef.onChange}
+              onBlur={professional.professionalIdRef.onBlur}
+              errors={professional.errors?.professionalId && true}
+              errorMessage={professional.errors?.professionalId?.message}
+              className={scss.nameInput}
+            />
+          )}
+          <label
+            htmlFor={`${professional.certifiedRef.name}`}
+            className={scss.checkboxContainer}
+          >
+            <input
+              onClick={() => professional.onCertifiedChange()}
+              type="checkbox"
+              id={`${professional.certifiedRef.name}`}
+              name={professional.certifiedRef.name}
+              className={scss.checkbox}
+            />
+            <span>Possui Curso Básico de Equoterapia</span>
+          </label>
+        </div>
+      </>
+    );
+  };
+
+  console.table(watch("fisioterapeuta"));
+  console.table(watch("psicologo"));
+  console.table(watch("medico"));
+  console.table(watch("equitador"));
+
   return (
     <main className={scss.main}>
       <div className={scss.container}>
@@ -102,157 +172,25 @@ export default function CadastroEquipe() {
           <h2 className={scss.subtitle}>Equipe Básica</h2>
 
           <div className={scss.inputGroups}>
-            <h3 className={scss.professionalTitle}>Fisioterapeuta</h3>
-            <Input
-              name={professionals.fisio.nameRef.name}
-              placeholder="Nome do Fisioterapeuta"
-              inputref={professionals.fisio.nameRef.ref}
-              value={watch("fisioterapeuta.nome")}
-              onChange={professionals.fisio.nameRef.onChange}
-              onBlur={professionals.fisio.nameRef.onBlur}
-              errors={errors.fisioterapeuta?.nome && true}
-              errorMessage={errors.fisioterapeuta?.nome?.message}
-            />
-
-            <div className={scss.inlineGroup}>
-              <Input
-                name={professionals.fisio.professionalIdRef.name}
-                placeholder="CREFITO"
-                inputref={professionals.fisio.professionalIdRef.ref}
-                value={watch("fisioterapeuta.professionalId")}
-                onChange={professionals.fisio.professionalIdRef.onChange}
-                onBlur={professionals.fisio.professionalIdRef.onBlur}
-                errors={errors.fisioterapeuta?.professionalId && true}
-                errorMessage={errors.fisioterapeuta?.professionalId?.message}
-                className={scss.nameInput}
-              />
-              <label htmlFor="checkbox" className={scss.checkboxContainer}>
-                <input
-                  onClick={onFisioCertifiedChange}
-                  type="checkbox"
-                  id="checkbox"
-                  name={professionals.fisio.certifiedRef.name}
-                  className={scss.checkbox}
-                />
-                <span>Possui Curso Básico de Equoterapia</span>
-              </label>
-            </div>
+            {renderInput(basicTeam.fisioterapeuta, "Fisioterapeuta")}
           </div>
 
           <div className={scss.divider} />
 
           <div className={scss.inputGroups}>
-            <h3 className={scss.professionalTitle}>Psicólogo</h3>
-            <Input
-              name={psicologoNameRef.name}
-              placeholder="Nome do Psicólogo"
-              inputref={psicologoNameRef.ref}
-              value={watch("psicologo.nome")}
-              onChange={psicologoNameRef.onChange}
-              onBlur={psicologoNameRef.onBlur}
-              errors={errors.psicologo?.nome && true}
-              errorMessage={errors.psicologo?.nome?.message}
-            />
-
-            <div className={scss.inlineGroup}>
-              <Input
-                name={psicologoProfessionalIdRef.name}
-                placeholder="CRP"
-                inputref={psicologoProfessionalIdRef.ref}
-                value={watch("psicologo.professionalId")}
-                onChange={psicologoProfessionalIdRef.onChange}
-                onBlur={psicologoProfessionalIdRef.onBlur}
-                errors={errors.psicologo?.professionalId && true}
-                errorMessage={errors.psicologo?.professionalId?.message}
-                className={scss.nameInput}
-              />
-              <label
-                onClick={onPsicoCertifiedChange}
-                htmlFor="checkbox"
-                className={scss.checkboxContainer}
-              >
-                <input
-                  type="checkbox"
-                  id="checkbox"
-                  name={psicologoCertifiedRef.name}
-                  className={scss.checkbox}
-                />
-                <span>Possui Curso Básico de Equoterapia</span>
-              </label>
-            </div>
+            {renderInput(basicTeam.psicologo, "Psicólogo")}
           </div>
 
           <div className={scss.divider} />
 
           <div className={scss.inputGroups}>
-            <h3 className={scss.professionalTitle}>Médico</h3>
-            <Input
-              name={medicoNameRef.name}
-              placeholder="Nome do Médico"
-              inputref={medicoNameRef.ref}
-              value={watch("medico.nome")}
-              onChange={medicoNameRef.onChange}
-              onBlur={medicoNameRef.onBlur}
-              errors={errors.medico?.nome && true}
-              errorMessage={errors.medico?.nome?.message}
-            />
-
-            <div className={scss.inlineGroup}>
-              <Input
-                name={medicoProfessionalIdRef.name}
-                placeholder="CRM"
-                inputref={medicoProfessionalIdRef.ref}
-                value={watch("medico.professionalId")}
-                onChange={medicoProfessionalIdRef.onChange}
-                onBlur={medicoProfessionalIdRef.onBlur}
-                errors={errors.medico?.professionalId && true}
-                errorMessage={errors.medico?.professionalId?.message}
-                className={scss.nameInput}
-              />
-              <label
-                onClick={onMedicoCertifiedChange}
-                htmlFor="checkbox"
-                className={scss.checkboxContainer}
-              >
-                <input
-                  type="checkbox"
-                  id="checkbox"
-                  name={medicoCertifiedRef.name}
-                  className={scss.checkbox}
-                />
-                <span>Possui Curso Básico de Equoterapia</span>
-              </label>
-            </div>
+            {renderInput(basicTeam.medico, "Médico")}
           </div>
 
           <div className={scss.divider} />
 
           <div className={scss.inputGroups}>
-            <h3 className={scss.professionalTitle}>Equitador</h3>
-            <Input
-              name={equitadorNameRef.name}
-              placeholder="Nome do Equitador"
-              inputref={equitadorNameRef.ref}
-              value={watch("equitador.nome")}
-              onChange={equitadorNameRef.onChange}
-              onBlur={equitadorNameRef.onBlur}
-              errors={errors.equitador?.nome && true}
-              errorMessage={errors.equitador?.nome?.message}
-            />
-
-            <label
-              onClick={onEquitadorCertifiedChange}
-              htmlFor="checkbox"
-              className={scss.checkboxContainer}
-            >
-              <input
-                type="checkbox"
-                id="checkbox"
-                name={equitadorCertifiedRef.name}
-                className={scss.checkbox}
-              />
-              <span>Possui Curso Básico de Equoterapia</span>
-            </label>
+            {renderInput(basicTeam.equitador, "Equitador")}
           </div>
         </form>
       </div>
