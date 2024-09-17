23e888857be16d1b674bea655274153c4989ec57
Yovany Cunha <yovany33@gmail.com>
2024-09-16 23:14:18 -0300

    activity services - sprint 12

--- src/services/activities/activities.ts
+++ src/services/activities/activities.ts
@@ -10,4 +10,49 @@ export class ActivitiesService {
       throw new Error("Erro ao buscar atividades");
     }
   };
+
+  static createActivity = async (activity: any) => {
+    try {
+      const { data } = await equocenterback.post(
+        "/api/activity/create",
+        activity
+      );
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw new Error("Erro ao criar atividade");
+    }
+  };
+
+  static getActivity = async (id: string) => {
+    try {
+      const { data } = await equocenterback.get(`/api/activity/${id}`);
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw new Error("Erro ao buscar atividade");
+    }
+  };
+
+  static updateActivity = async (activity: any) => {
+    try {
+      const { data } = await equocenterback.patch(
+        `/api/activity/update`,
+        activity
+      );
+    } catch (error) {
+      console.error(error);
+      throw new Error("Erro ao atualizar atividade");
+    }
+  };
+
+  static removeActivity = async (id: string) => {
+    try {
+      const { data } = await equocenterback.delete(`/api/activity/${id}`);
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw new Error("Erro ao remover atividade");
+    }
+  };
 }
